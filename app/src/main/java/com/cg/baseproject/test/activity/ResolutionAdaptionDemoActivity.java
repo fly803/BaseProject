1fa5089de74077b3bbea5627248218dd4858fb9c
fly803 <13521549681>
2018-03-29 22:59:56 +0800

    北里电脑AndroidStudio3.1可以编译运行通过

--- app/src/main/java/com/cg/baseproject/test/activity/ResolutionAdaptionDemoActivity.java
+++ app/src/main/java/com/cg/baseproject/test/activity/ResolutionAdaptionDemoActivity.java
@@ -7,7 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.ivy.baseproject.test.R;
+import com.cg.baseproject.test.R;
+
 
 public class ResolutionAdaptionDemoActivity extends AppCompatActivity {
 
@@ -15,16 +16,6 @@ public class ResolutionAdaptionDemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resolution_adaption_demo);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
     }
 
 }
