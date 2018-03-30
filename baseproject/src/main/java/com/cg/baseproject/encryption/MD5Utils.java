e1ba0bfc2f41aba709309ea4f804f13acf105320
fly803 <13521549681>
2018-03-30 18:32:18 +0800

    增加加密算法注释

--- baseproject/src/main/java/com/cg/baseproject/encryption/MD5Utils.java
+++ baseproject/src/main/java/com/cg/baseproject/encryption/MD5Utils.java
@@ -15,6 +15,16 @@ import java.security.NoSuchAlgorithmException;
  */
 
 public class MD5Utils {
+    /**
+     * 不可逆加密算法:不可逆加密算法的特征是加密过程中不需要使用密钥，输入明文后由系统直接经过加密算法处理成密文，这种加密后的数据是无法被解密的，
+     * 只有重新输入明文，并再次经过同样不可逆的加密算法处理，得到相同的加密密文并被系统重新识别后，才能真正解密。显然，在这类加密过程中，加密是自
+     * 己，解密还得是自己，而所谓解密，实际上就是重新加一次密，所应用的“密码”也就是输入的明文。不可逆加密算法不存在密钥保管和分发问题，非常适合在
+     * 分布式网络系统上使用，但因加密计算复杂，工作量相当繁重，通常只在数据量有限的情形下使用，如广泛应用在计算机系统中的口令加密，利用的就是不可
+     * 逆加密算法。近年来，随着计算机系统性能的不断提高，不可逆加密的应用领域正在逐渐增大。在计算机网络中应用较多不可逆加密算法的有RSA公司发明的
+     * MD5算法和由美国国家标准局建议的不可逆加密标准SHS(Secure Hash Standard:安全杂乱信息标准)等
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         // TODO Auto-generated method stub  
         System.out.println("加密后："+encryptMD5("cg"));
--- baseproject/src/main/java/com/cg/baseproject/encryption/RSAUtils.java
+++ baseproject/src/main/java/com/cg/baseproject/encryption/RSAUtils.java
@@ -4,7 +4,6 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import android.util.Base64;
 
 import javax.crypto.Cipher;
 
@@ -12,6 +11,15 @@ import javax.crypto.Cipher;
  * @author
  * @version 1.0
  * @date 2018/3/12
+ * 加密算法可以分为对称加密、不对称加密和不可逆加密
+ * http://desert3.iteye.com/blog/743713
+ *
+    不对称加密算法:不对称加密算法使用两把完全不同但又是完全匹配的一对钥匙—公钥和私钥。在使用不对称加密算法加密文件时，只有使用匹配的一对公钥和私钥，
+    才能完成对明文的加密和解密过程。加密明文时采用公钥加密，解密密文时使用私钥才能完成，而且发信方（加密者）知道收信方的公钥，只有收信方（解密者）
+    才是唯一知道自己私钥的人。不对称加密算法的基本原理是，如果发信方想发送只有收信方才能解读的加密信息，发信方必须首先知道收信方的公钥，然后利用
+    收信方的公钥来加密原文；收信方收到加密密文后，使用自己的私钥才能解密密文。显然，采用不对称加密算法，收发信双方在通信之前，收信方必须将自己早
+    已随机生成的公钥送给发信方，而自己保留私钥。由于不对称算法拥有两个密钥，因而特别适用于分布式系统中的数据加密。广泛应用的不对称加密算法有RSA
+    算法和美国国家标准局提出的DSA(Digital Signature Algorithm)。以不对称加密算法为基础的加密技术应用非常广泛。 
  */
 
 public class RSAUtils {
