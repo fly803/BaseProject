79f25de83e9935a407fba1cc86d54cb4402cb61f
wixche <wixhe@126.com>
2018-05-21 13:29:43 +0800

    修复点击事件无响应问题.

--- app/src/main/java/com/ivy/baseproject/test/sample/SampleFragment.java
+++ app/src/main/java/com/ivy/baseproject/test/sample/SampleFragment.java
@@ -2,9 +2,7 @@ package com.ivy.baseproject.test.sample;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,25 +10,18 @@ import com.cg.baseproject.base.BaseSupportFragment;
 import com.cg.baseproject.configs.IConstants;
 import com.ivy.baseproject.test.R;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
-
 /**
  * 时 间: 2017/3/18
  */
 
 public class SampleFragment extends BaseSupportFragment {
 
-    @BindView(R.id.tvFragmentSample)
     TextView mTvFragmentSample;
 
     @Override
     protected void onClickFailureResetButton(View view) {
         super.onClickFailureResetButton(view);
         Toast.makeText(getActivity(), "网络错误从新加载", Toast.LENGTH_SHORT).show();
-        
     }
 
     @Override
@@ -51,7 +42,14 @@ public class SampleFragment extends BaseSupportFragment {
 
     @Override
     protected View getSuccessView() {
-        View view = View.inflate(getActivity(), R.layout.fragment_sample, null);
+        final View view = View.inflate(getActivity(), R.layout.fragment_sample, null);
+        mTvFragmentSample = view.findViewById(R.id.tvFragmentSample);
+        mTvFragmentSample.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "SampleFragment点击测试", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 
@@ -63,16 +61,17 @@ public class SampleFragment extends BaseSupportFragment {
     @Override
     protected Object requestData() {
         new Handler().postDelayed(new Runnable(){
+            @Override
             public void run() {
                 //execute the task   
-                refreshPage(IConstants.STATE_FAILED);
+                refreshPage(IConstants.STATE_SUCCESSED);
             }
         }, 3000);
         return IConstants.STATE_LOADING;
     }
 
-    @OnClick(R.id.tvFragmentSample)
-    public void onViewClicked() {
-        Toast.makeText(getActivity(), "SampleFragment点击测试", Toast.LENGTH_SHORT).show();
-    }
+//    @OnClick(R.id.tvFragmentSample)
+//    public void onViewClicked() {
+//        Toast.makeText(getActivity(), "SampleFragment点击测试", Toast.LENGTH_SHORT).show();
+//    }
 }
