d8c3d002a187f852b2f5ee6554ffa51ab6e93ebc
fly803 <13521549681>
2018-05-24 10:54:13 +0800

    BaseProjectConfig增加内存泄漏，全局异常捕捉配置

--- baseproject/src/main/java/com/cg/baseproject/configs/BaseProjectConfig.java
+++ baseproject/src/main/java/com/cg/baseproject/configs/BaseProjectConfig.java
@@ -1,5 +1,10 @@
 package com.cg.baseproject.configs;
 
+import android.app.Application;
+
+import com.cg.baseproject.crash.CrashHandler;
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * @author
  * @version 1.0
@@ -9,14 +14,18 @@ package com.cg.baseproject.configs;
 public class BaseProjectConfig {
     public static  int SUCCESS_CODE = 0;
     public static  boolean DEBUG = true;
-    public static  boolean ISLEAKCANARY = true;
     public static  String BASE_URL = "http://ip.taobao.com/";
     public static  String TAG = "retrofit";
     public static final String DOUBAN_BASE_URL = "https://api.douban.com/";
     public static final String GANK_BASE_URL = "https://gank.io/";
-    
-    public static void init(boolean isLeakCanary,boolean debug,String baseurl,int successcode,String tag){
-        ISLEAKCANARY = isLeakCanary;
+    public static void init(Application application, boolean isLeakCanary,boolean isCrashHandel, boolean debug, String baseurl, int successcode, String tag){
+        if(isLeakCanary){
+            LeakCanary.install(application);
+        }
+        if(isCrashHandel){
+            CrashHandler crashHandler = CrashHandler.getInstance();
+            crashHandler.init(application);
+        }
         DEBUG = debug;
         BASE_URL = baseurl;
         SUCCESS_CODE = successcode;
