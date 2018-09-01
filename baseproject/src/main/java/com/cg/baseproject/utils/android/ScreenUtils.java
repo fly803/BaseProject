9d6ed913133c80807072ee917584af57514fd054
fly803 <13521549681>
2018-09-01 11:17:29 +0800

    添加setCustomDensity

--- baseproject/src/main/java/com/cg/baseproject/utils/android/ScreenUtils.java
+++ baseproject/src/main/java/com/cg/baseproject/utils/android/ScreenUtils.java
@@ -1,7 +1,9 @@
 package com.cg.baseproject.utils.android;
 
 import android.app.Activity;
+import android.app.Application;
 import android.app.KeyguardManager;
+import android.content.ComponentCallbacks;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
@@ -349,5 +351,49 @@ public final class ScreenUtils {
         }
         activityDm.scaledDensity = activityDm.density * (appDm.scaledDensity / appDm.density);
         activityDm.densityDpi = (int) (160 * activityDm.density);
+    }
+
+    /**
+     * 适配：修改设备密度
+     * DisplayUtil.setCustomDensity(this, getApplication());使用方法
+     */
+    private static float sNoncompatDensity;
+    private static float sNoncompatScaledDensity;
+    
+
+    public static void setCustomDensity(@NonNull Activity activity, @NonNull final Application application) {
+        DisplayMetrics appDisplayMetrics = application.getResources().getDisplayMetrics();
+        if (sNoncompatDensity == 0) {
+            sNoncompatDensity = appDisplayMetrics.density;
+            sNoncompatScaledDensity = appDisplayMetrics.scaledDensity;
+            // 防止系统切换后不起作用
+            application.registerComponentCallbacks(new ComponentCallbacks() {
+                @Override
+                public void onConfigurationChanged(Configuration newConfig) {
+                    if (newConfig != null && newConfig.fontScale > 0) {
+                        sNoncompatScaledDensity = application.getResources().getDisplayMetrics().scaledDensity;
+                    }
+                }
+
+                @Override
+                public void onLowMemory() {
+
+                }
+            });
+        }
+        float targetDensity = appDisplayMetrics.widthPixels / 360;
+        // 防止字体变小
+        float targetScaleDensity = targetDensity * (sNoncompatScaledDensity / sNoncompatDensity);
+        int targetDensityDpi = (int) (160 * targetDensity);
+
+        appDisplayMetrics.density = targetDensity;
+        appDisplayMetrics.scaledDensity = targetScaleDensity;
+        appDisplayMetrics.densityDpi = targetDensityDpi;
+
+        final DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
+        activityDisplayMetrics.density = targetDensity;
+        activityDisplayMetrics.scaledDensity = targetScaleDensity;
+        activityDisplayMetrics.densityDpi = targetDensityDpi;
+
     }
 }
